6f4133f44c79cf52cf6e4aad4af6d1fe21c22ad1
dimon-mirchuk <dimon.mirchuk@gmail.com>
2021-09-03 18:41:07 +0300

    Remade timer

--- script.js
+++ script.js
@@ -1,16 +1,38 @@
-const refs = {
-  selector: document.querySelector("#timer-1"),
-  daysEl: document.querySelector('span[data-value="days"]'),
-  hoursEl: document.querySelector('span[data-value="hours"]'),
-  minsEl: document.querySelector('span[data-value="mins"]'),
-  secsEl: document.querySelector('span[data-value="secs"]'),
-};
-
 class CountdownTimer {
   constructor({ selector, targetDate }) {
     this.selector = selector;
     this.targetDate = targetDate;
     this.start();
+    this.markup = `
+      <div class="field">
+        <span class="value" data-value="days">11</span>
+        <span class="label">Days</span>
+      </div>
+
+      <div class="field">
+        <span class="value" data-value="hours">11</span>
+        <span class="label">Hours</span>
+      </div>
+
+      <div class="field">
+        <span class="value" data-value="mins">11</span>
+        <span class="label">Minutes</span>
+      </div>
+
+      <div class="field">
+        <span class="value" data-value="secs">11</span>
+        <span class="label">Seconds</span>
+      </div>`;
+
+    this.root = document.querySelector(this.selector);
+    this.root.insertAdjacentHTML("beforeend", this.markup);
+
+    this.refs = {
+      daysEl: this.root.querySelector('span[data-value="days"]'),
+      hoursEl: this.root.querySelector('span[data-value="hours"]'),
+      minsEl: this.root.querySelector('span[data-value="mins"]'),
+      secsEl: this.root.querySelector('span[data-value="secs"]'),
+    };
   }
 
   init() {
@@ -29,10 +51,10 @@ class CountdownTimer {
   }
 
   updateClockface({ days, hours, mins, secs }) {
-    refs.daysEl.textContent = `${days}`;
-    refs.hoursEl.textContent = `${hours}`;
-    refs.minsEl.textContent = `${mins}`;
-    refs.secsEl.textContent = `${secs}`;
+    this.refs.daysEl.textContent = `${days}`;
+    this.refs.hoursEl.textContent = `${hours}`;
+    this.refs.minsEl.textContent = `${mins}`;
+    this.refs.secsEl.textContent = `${secs}`;
   }
 
   getTimeComponents(time) {
@@ -50,7 +72,12 @@ class CountdownTimer {
   }
 }
 
-const timer = new CountdownTimer({
+const firstTimer = new CountdownTimer({
   selector: "#timer-1",
   targetDate: new Date("Sept 17, 2021"),
+});
+
+const secondTimer = new CountdownTimer({
+  selector: "#timer-2",
+  targetDate: new Date("Jan 01, 2022"),
 });
